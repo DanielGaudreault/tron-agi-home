74f72ea237feca44f5514770e361537003884d42
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-05 23:11:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     const tronGrid = document.querySelector('.tron-grid');
-
-    // Generate Tron grid cells dynamically
-    for (let i = 0; i < 400; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        tronGrid.appendChild(cell);
-    }
-});
+    const userInput = document.getElementById('user-input');
+    const sendButton = document
