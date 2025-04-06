4947e3d4b7cd330cef0a954ca4e2968d2e25b8e6
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-05 23:21:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     const tronGrid = document.querySelector('.tron-grid');
     const userInput = document.getElementById('user-input');
+    const send
