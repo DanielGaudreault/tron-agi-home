94bea4ed3999eb8718d3eb1ce4ca374c50764949
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-06 20:07:58 -0400

    Update tf-agent.js

--- tf-agent.js
+++ tf-agent.js
@@ -1,1 +1,20 @@
+// ===== AI AGENT (TensorFlow.js) =====
+class NeuralAgent {
+    constructor() {
+        this.model = tf.sequential();
+        this.model.add(tf.layers.dense({ units: 16, inputShape: [4], activation: "relu" }));
+        this.model.add(tf.layers.dense({ units: 8, activation: "relu" }));
+        this.model.add(tf.layers.dense({ units: 4, activation: "softmax" }));
+        this.model.compile({ optimizer: "adam", loss: "meanSquaredError" });
+    }
+    predict(state) {
+        return tf.tidy(() => {
+            const input = tf.tensor2d([state]);
+            return this.model.predict(input).dataSync();
+        });
+    }
+}
 
+// Example: Use this to control an ISO
+// const aiAgent = new NeuralAgent();
+// const action = aiAgent.predict([iso.x, iso.y, user.x, user.y]);
