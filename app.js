b77087e5b9a1d17c9958fcca5cb897d4c40399ab
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-06 21:21:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,4 @@ function App() {
   );
 }
 
-export default App;import React from 'react';
-import World from './components/World';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Tron World</h1>
-      </header>
-      <World />
-    </div>
-  );
-}
-
 export default App;
