a6c27300f938e1dfd73bd55a2aceb9c0068d6579
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-06 21:15:37 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import World from './components/World';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Tron World</h1>
+      </header>
+      <World />
+    </div>
+  );
+}
+
+export default App;import React from 'react';
+import World from './components/World';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Tron World</h1>
+      </header>
+      <World />
+    </div>
+  );
+}
+
+export default App;
